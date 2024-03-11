f371d68510dbc89126204f72bbd41fd1cbfee310
MiguelViapiana <127536583+MiguelViapiana@users.noreply.github.com>
2024-03-11 11:15:21 -0300

    Exercicio versão 04

---
+++ Exercicio04.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Exercicio04 {
+    public static void executar(){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite dois números: ");
+        double num1, num2;
+        num1 = sc.nextDouble();
+        num2 = sc.nextDouble();
+
+        System.out.println("A soma é: "+ (num1+num2));
+        System.out.println("A Subtração é: "+ (num1-num2));
+        System.out.println("A Multiplicação é: "+ num1*num2);
+        System.out.println("A Divisão é: "+ num1/num2);
+    }
+}
