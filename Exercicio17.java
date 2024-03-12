06ae61b606ec7ba5dfc60c5d83c85ec414423a8d
Miguel Viapiana Jung <127536583+MiguelViapiana@users.noreply.github.com>
2024-03-11 22:38:53 -0300

    Exercicio versão 17

--- Exercicio17.java
+++ Exercicio17.java
@@ -1,5 +1,14 @@
 public class Exercicio17 {
     public static void executar(){
-        
+        System.out.println("--- Média ponderada --- \nDigite as três notas: ");
+        double nota1 = Prompt.lerDecimal(" 1° Nota: ");
+        double nota2 = Prompt.lerDecimal(" 2° Nota: ");
+        double nota3 = Prompt.lerDecimal(" 3° Nota: ");
+        int peso1 = Prompt.lerInteiro(" Peso 1: ");
+        int peso2 = Prompt.lerInteiro(" Peso 2: ");
+        int peso3 = Prompt.lerInteiro(" Peso 3: ");
+
+        double mediaPond = (nota1 * peso1 + nota2 * peso2 + nota3 * peso3) / (peso1 + peso2 + peso3);
+        System.out.printf("A média ponderada das notas é de: %.2f", mediaPond);
     }
 }
--- Program.java
+++ Program.java
@@ -17,7 +17,8 @@ public class Program{
     //Exercicio13.executar();
     //Exercicio14.executar();
     //Exercicio15.executar();
-    Exercicio16.executar();
+    //Exercicio16.executar();
+    Exercicio17.executar();
 
 
     }
